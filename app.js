17fa96ce5ab03b90b7c3034e5ef8a59f2530efe9
Yagho Vilela <yagho05@gmail.com>
2022-08-02 12:57:29 -0300

    corrigindo o app js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("Aplicação está se conectando ao banco de dados")
+console.log("O Paulo fez esse commit sem você ver na aula!")
+console.log("Enviando para o GitHub")
